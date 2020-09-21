0248c550bce9fc3ab77222d70bd747a60b545198
quantran <trtquan@gmail.com>
2020-09-21 08:25:39 +0700

    update-language

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/whoami", (req, res) => {
   res.json({
     "ipaddress": req.ip,
-    "language": req.language,
-    "software": "init"
+    "language": req.headers["accept-language"],
+    "software": "req"
   })
 })
 
