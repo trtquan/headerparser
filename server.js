995896b6153cc27c7e07cb3e1a9d4fe97c787abc
quantran <trtquan@gmail.com>
2020-09-21 08:24:11 +0700

    update-language

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/whoami", (req, res) => {
   res.json({
     "ipaddress": req.ip,
-    "language": "vn",
+    "language": req.language,
     "software": "init"
   })
 })
